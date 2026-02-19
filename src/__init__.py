789d33c12ce23a9322aa4701c8b6074b98a8da3e
subbaramireddyk <subbarami.reddy@oneadvanced.com>
2026-02-19 14:49:05 +0530

    Apply black formatting to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,11 @@ __author__ = "EKS Upgrade Planner Contributors"
 
 from src.utils import setup_logger, get_logger
 from src.scanner import EKSScanner, K8sScanner
-from src.analyzer import CompatibilityAnalyzer, DeprecationAnalyzer, ReleaseNotesAnalyzer
+from src.analyzer import (
+    CompatibilityAnalyzer,
+    DeprecationAnalyzer,
+    ReleaseNotesAnalyzer,
+)
 from src.planner import UpgradePathPlanner, RiskAssessment, MigrationPlanner
 from src.reporter import MarkdownReporter, JSONExporter, HTMLReporter
 
